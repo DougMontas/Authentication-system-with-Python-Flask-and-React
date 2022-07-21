9521ac3ec00e4e4db112f3fa93c9cc1664e1ad46
DougMontas <dougmontas@gmail.com>
2022-07-21 15:55:44 +0000

    added project

--- src/api/models.py
+++ src/api/models.py
@@ -6,11 +6,11 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(250), unique=False, nullable=True)
+    password = db.Column(db.String(275), unique=False, nullable=True)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<Favorites {self.email}>'
 
     def serialize(self):
         return {
@@ -18,6 +18,8 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+# _______________________________________________________________________________________________________
 
 
 class Account(db.Model):
@@ -33,4 +35,154 @@ class Account(db.Model):
         return {
             'id': self.id,
             'total': self.total
+        }
+# _______________________________________________________________________________________________________
+
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    height = db.Column(db.Integer, unique=False, nullable=True)
+    mass = db.Column(db.Integer, unique=False, nullable=True)
+    hair_color = db.Column(db.String(120), unique=False, nullable=True)
+    skin_color = db.Column(db.String(120), unique=False, nullable=True)
+    eye_color = db.Column(db.String(120), unique=False, nullable=True)
+    birth_year = db.Column(db.Integer, unique=False, nullable=True)
+    gender = db.Column(db.String(120), unique=False, nullable=True)
+    homeworld = db.Column(db.String(120), unique=False, nullable=True)
+
+    def __repr__(self):
+        return f"<People {self.id}>"
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'height': self.height,
+            'mass': self.mass,
+            'skin_color': self.skin_color,
+            'eye_color': self.eye_color,
+            'birth_year': self.birth_year,
+            'gender': self.gender,
+            'homeworld': self.homeworld
+        }
+# _______________________________________________________________________________________________________
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    diameter = db.Column(db.Integer, unique=False, nullable=True)
+    rotation_period = db.Column(db.Integer, unique=False, nullable=True)
+    orbital_period = db.Column(db.Integer, unique=False, nullable=True)
+    gravity = db.Column(db.String(120), unique=False, nullable=True)
+    population = db.Column(db.Integer, unique=False, nullable=True)
+    climate = db.Column(db.String(120), unique=False, nullable=True)
+    terrain = db.Column(db.String(120), unique=False, nullable=True)
+    surface_water = db.Column(db.Integer, unique=False, nullable=True)
+    url = db.Column(db.String(120), unique=False, nullable=True)
+
+    def __repr__(self):
+        return f"<Planet {self.id}>"
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'diameter': self.diameter,
+            'rotation_period': self.rotation_period,
+            'orbital_period': self.orbital_period,
+            'gravity': self.gravity,
+            'population': self.population,
+            'climate': self.climate,
+            'terrain': self.terrain,
+            'surface_water': self.surface_water,
+            'url': self.url
+        }
+# _______________________________________________________________________________________________________
+
+
+class Species(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    classification = db.Column(db.String(120), unique=False, nullable=True)
+    designation = db.Column(db.String(120), unique=False, nullable=True)
+    average_height = db.Column(db.Integer, unique=False, nullable=True)
+    skin_color = db.Column(db.String(120), unique=False, nullable=True)
+    hair_color = db.Column(db.String(120), unique=False, nullable=True)
+    eye_color = db.Column(db.String(120), unique=False, nullable=True)
+    average_lifespan = db.Column(db.Integer, unique=False, nullable=True)
+    homeworld = db.Column(db.String(120), unique=False, nullable=True)
+    language = db.Column(db.String(120), unique=False, nullable=True)
+
+    def __repr__(self):
+        return f"<Species {self.id}>"
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'classification': self.classification,
+            'designation': self.designation,
+            'average_height': self.average_height,
+            'skin_colors': self.skin_colors,
+            'hair_colors': self.hair_colors,
+            'eye_colors': self.eye_colors,
+            'average_lifespan': self.average_lifespan,
+            'homeworld': self.homeworld,
+            'language': self.language
+        }
+# _______________________________________________________________________________________________________
+
+
+class Vehicles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    model = db.Column(db.String(120), unique=False, nullable=True)
+    manufacturer = db.Column(db.String(120), unique=False, nullable=True)
+    cost_in_credits = db.Column(db.Integer, unique=False, nullable=True)
+    length = db.Column(db.Integer, unique=False, nullable=True)
+    max_atmosphering_speed = db.Column(db.Integer, unique=False, nullable=True)
+    crew = db.Column(db.Integer, unique=False, nullable=True)
+    passengers = db.Column(db.Integer, unique=False, nullable=True)
+    cargo_capacity = db.Column(db.Integer, unique=False, nullable=True)
+    consumables = db.Column(db.String(120), unique=False, nullable=True)
+    vehicle_class = db.Column(db.String(120), unique=False, nullable=True)
+    pilots = db.Column(db.String(120), unique=False, nullable=True)
+
+    def __repr__(self):
+        return f"<Vehicles {self.id}>"
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "vehicle_class": self.vehicle_class,
+            "pilots": self.pilots,
+        }
+# _______________________________________________________________________________________________________
+
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(275), unique=False, nullable=True)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Account
+from api.models import db, User, Account, People, Planet, Species, Vehicles, Favorites
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from argon2 import PasswordHasher
@@ -25,6 +25,7 @@ def handle_hello():
 @api.route('/register', methods=['POST'])
 def register():
     payload = request.get_json()
+    print(ph.hash(payload['password']))
 
     user = User(
         email=payload['email'],
@@ -36,38 +37,151 @@ def register():
     db.session.commit()
 
     return "user registered", 200
+# ____________________________________________________________________
 
 
 @api.route('/login', methods=['POST'])
 def login():
     payload = request.get_json()
+
     user = User.query.filter(User.email == payload['email']).first()
     if user is None:
         return 'failed-auth', 401
+        # return 'user not found', 401
+    # hash = ph.hash(payload['password'])
+    # print(user.password)
+    # print(hash)
 
     try:
         ph.verify(user.password, payload['password'])
+        # ph.verify(user.password, ph.hash(payload['password']))
+
     except:
+        # print(user.password)
+        # print(hash)
+        # return 'incorrect password', 401
         return 'failed-auth', 401
 
     token = create_access_token(identity=user.id)
-    return jsonify(({'token': token}))
+
+    return jsonify({'token': token})
+
+# ____________________________________________________________________
 
 
-@api.route('/accounts', methods=['GET'])
-@jwt_required()
+@ api.route('/accounts', methods=['GET'])
+@ jwt_required()
 def accounts():
     user_id = get_jwt_identity()
 
     user = User.query.get(user_id)
-    accounts = Account.query.filter(Account.user_id == user.id)
+    accounts = Account.query.filter(Account.user_id == user_id).all()
 
     account_info = {
-        "accounts": [
-            {"account": "0009001", "total": "$100.00"},
-            {"account": "0009002", "total": "$99.00"},
-        ],
+        "accounts": [x.serialize() for x in accounts],
         "user": user.serialize()
     }
 
     return jsonify(account_info)
+
+
+@api.route('/account/<int:id>', methods=['DELETE'])
+def del_account(id):
+    account_data = Account.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return 'Account deleted', 204
+
+
+# ____________________________________________________________________
+
+
+@api.route('/people', methods=['GET'])
+def people():
+
+    people_data = People.query.all()
+
+    response_body = [person.serialize() for person in people_data]
+
+    return jsonify(response_body), 200
+
+
+@api.route('/people/<int:id>', methods=['DELETE'])
+def del_people(id):
+    people_data = People.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return 'People deleted', 204
+
+# ____________________________________________________________________
+
+
+@api.route('/favorites', methods=['GET'])
+def favorites():
+
+    favorites_data = Favorites.query.all()
+    response_body = [favorites.serialize() for favorite in favorites_data]
+    return jsonify(response_body), 200
+
+
+@api.route('/favorites/<int:id>', methods=['DELETE'])
+def del_favorites(id):
+    favorites_data = Favorites.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return 'Favorites deleted', 204
+
+# ____________________________________________________________________
+
+
+@api.route('/planet', methods=['GET'])
+def planet():
+
+    planet_data = Planet.query.all()
+
+    response_body = [planet.serialize() for planet in planet_data]
+
+    return jsonify(response_body), 200
+
+
+@api.route('/planet/<int:id>', methods=['DELETE'])
+def del_planet(id):
+    planet_data = Planet.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return 'Planet deleted', 204
+
+# ____________________________________________________________________
+
+
+@api.route('/species', methods=['GET'])
+def species():
+    species_data = Species.query.all()
+    response_body = [species.serialize() for species in species_data]
+    return jsonify(response_body), 200
+
+
+@api.route('/species/<int:id>', methods=['DELETE'])
+def del_species(id):
+    species_data = Species.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return 'Species deleted', 204
+
+
+# ____________________________________________________________________
+
+
+@api.route('/vehicles', methods=['GET'])
+def vehicles():
+    vehicles_data = Vehicles.query.all()
+    response_body = [vehicles.serialize() for vehicles in vehicles_data]
+    return jsonify(response_body), 200
+
+
+@api.route('/vehicles/<int:id>', methods=['DELETE'])
+def del_vehicles(id):
+    vehicles_data = Vehicles.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return 'Vehicles deleted', 204
